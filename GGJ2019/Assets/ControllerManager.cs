27cd1ee2f05e0ad646210abc46539aa24bd51394
Gaz Robinson <gaz.j.robinson@gmail.com>
2019-01-26 12:34:05 +0000

    My stuff

---
+++ GGJ2019/Assets/ControllerManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Button {
+    A,
+    B,
+    START,
+    L,
+    R,
+    C_UP,
+    C_RIGHT,
+    C_DOWN,
+    C_LEFT
+}
+
+public class ControllerManager : MonoBehaviour {
+    public Transform controlStick;
+    public Transform dPad;
+
+    public ControllerButton A_Button;
+    public ControllerButton B_Button;
+    public ControllerButton Start_Button;
+
+    public ControllerButton C_UP;
+    public ControllerButton C_RIGHT;
+    public ControllerButton C_DOWN;
+    public ControllerButton C_LEFT;
+
+
+    private float maxStickTilt = 20.0f;
+    private float maxPadTilt = 5.0f;
+
+    private Transform currentButton;
+    private Vector3 restPos;
+    float buttonT = 0f;
+
+    public void SetStick( Vector2 input ) {
+        controlStick.localRotation = Quaternion.Euler( maxStickTilt * -input.y, 0.0f, maxStickTilt * input.x );
+    }
+
+    public void SetDPad( Vector2 input ) {
+        dPad.localRotation = Quaternion.Euler( maxPadTilt * -input.y, 0.0f, maxPadTilt * input.x );
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( currentButton != null ) {
+
+        }
+	}
+    public void PressButton(Button btn) {
+        switch ( btn ) {
+            case Button.A:
+                A_Button.Press();
+                break;
+            case Button.B:
+                B_Button.Press();
+                break;
+            case Button.START:
+                Start_Button.Press();
+                break;
+            case Button.C_UP:
+                C_UP.Press();
+                break;
+            case Button.C_RIGHT:
+                C_RIGHT.Press();
+                break;
+            case Button.C_DOWN:
+                C_DOWN.Press();
+                break;
+            case Button.C_LEFT:
+                C_LEFT.Press();
+                break;
+        }
+    }
+    public void ReleaseButton( Button btn ) {
+        switch ( btn ) {
+            case Button.A:
+                A_Button.Release();
+                break;
+            case Button.B:
+                B_Button.Release();
+                break;
+            case Button.START:
+                Start_Button.Release();
+                break;
+            case Button.C_UP:
+                C_UP.Release();
+                break;
+            case Button.C_RIGHT:
+                C_RIGHT.Release();
+                break;
+            case Button.C_DOWN:
+                C_DOWN.Release();
+                break;
+            case Button.C_LEFT:
+                C_LEFT.Release();
+                break;
+        }
+    }
+}
---
+++ GGJ2019/Assets/HandManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour {
+    public ControllerManager c_manager;
+
+    private Transform LeftHand, RightHand;
+    private float stickOffset = 0.04f, padOffset = 0.04f;
+    Transform StickAnchor;
+    Transform DPadAnchor;
+
+	// Use this for initialization
+	void Start () {
+        LeftHand = transform.Find( "Paw_left" );
+        RightHand = transform.Find( "Paw_right" );
+        StickAnchor = transform.Find( "StickAnchor" );
+        DPadAnchor = transform.Find( "PadAnchor" );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 stickInput = new Vector2( Input.GetAxis( "Horizontal" ), Input.GetAxis( "Vertical" ) );
+        Vector2 dPadInput = new Vector2( Input.GetAxis( "DPad_H" ), Input.GetAxis( "DPad_V" ) );
+        print( stickInput.y );
+        if ( stickInput.sqrMagnitude > 0 ) {
+            LeftHand.localPosition = StickAnchor.localPosition + ( Vector3 )( stickInput * stickOffset );
+            c_manager.SetStick( stickInput );
+            c_manager.SetDPad( Vector2.zero );
+        }
+        else if ( dPadInput.sqrMagnitude > 0 ) {
+            LeftHand.localPosition = DPadAnchor.localPosition + ( Vector3 )( dPadInput * padOffset );
+            c_manager.SetDPad( dPadInput );
+            c_manager.SetStick( Vector2.zero );
+        }
+        else {
+            LeftHand.localPosition = StickAnchor.localPosition;
+            c_manager.SetStick( Vector2.zero );
+            c_manager.SetDPad( Vector2.zero );
+
+        }
+        GetButtons();
+	}
+    void GetButtons() {
+        //Down
+        if ( Input.GetButtonDown( "A" ) )
+            c_manager.PressButton( Button.A );
+        if ( Input.GetButtonDown( "B" ) )
+            c_manager.PressButton( Button.B );
+        if ( Input.GetButtonDown( "START" ) )
+            c_manager.PressButton( Button.START );
+        if ( Input.GetButtonDown( "C_UP" ) )
+            c_manager.PressButton( Button.C_UP );
+        if ( Input.GetButtonDown( "C_RIGHT" ) )
+            c_manager.PressButton( Button.C_RIGHT );
+        if ( Input.GetButtonDown( "C_DOWN" ) )
+            c_manager.PressButton( Button.C_DOWN );
+        if ( Input.GetButtonDown( "C_LEFT" ) )
+            c_manager.PressButton( Button.C_LEFT );
+
+        //Up
+        if ( Input.GetButtonUp( "A" ) )
+            c_manager.ReleaseButton( Button.A );
+        if ( Input.GetButtonUp( "B" ) )
+            c_manager.ReleaseButton( Button.B );
+        if ( Input.GetButtonUp( "START" ) )
+            c_manager.ReleaseButton( Button.START );
+        if ( Input.GetButtonUp( "C_UP" ) )
+            c_manager.ReleaseButton( Button.C_UP );
+        if ( Input.GetButtonUp( "C_RIGHT" ) )
+            c_manager.ReleaseButton( Button.C_RIGHT );
+        if ( Input.GetButtonUp( "C_DOWN" ) )
+            c_manager.ReleaseButton( Button.C_DOWN );
+        if ( Input.GetButtonUp( "C_LEFT" ) )
+            c_manager.ReleaseButton( Button.C_LEFT );
+    }
+}
